b2a30c5e8391f2af22dc6dfffcdfe7771fd07e9e
tsitsianov <34614508+tsitsianov@users.noreply.github.com>
2019-01-11 12:00:25 +1000

    Create non-unique-elements.py

---
+++ Home/Non-unique Elements/non-unique-elements.py
@@ -0,0 +1,28 @@
+#Your optional code here
+#You can import some modules or create additional functions
+
+
+def checkio(data):
+    new_list = []
+    for i in data:
+        if data.count(i) > 1:
+            new_list.append(i)
+    #Your code here
+    #It's main function. Don't remove this function
+    #It's used for auto-testing and must return a result for check.  
+
+    #replace this for solution
+    return new_list
+
+#Some hints
+#You can use list.count(element) method for counting.
+#Create new list with non-unique elements
+#Loop over original list
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
